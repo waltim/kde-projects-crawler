457dbf8e2a6296705574abc52fafe68b26e8f1d4
waltim <waltimlmm@gmail.com>
2022-05-20 13:59:27 -0300

    configurando busca pelo ano do primeiro commit por repositório

--- src/main/java/br/com/unb/cic/application/Program.java
+++ src/main/java/br/com/unb/cic/application/Program.java
@@ -9,22 +9,23 @@ import java.util.ArrayList;
 
 import br.com.unb.cic.entities.Project;
 import br.com.unb.cic.enums.IOEnum;
+import br.com.unb.cic.scrapy.ProjectCrawler;
 import br.com.unb.cic.scrapy.ProjectScrapper;
 
 public class Program {
 
 	public static void main(String[] args) throws InterruptedException {
 
-//		ArrayList<String> repositories = ProjectCrawler.crawler("https://github.com/orgs/KDE/repositories", "KDE");
+		ArrayList<String> repositories = ProjectCrawler.crawler("https://github.com/orgs/KDE/repositories", "KDE");
 
-		ArrayList<String> repositories = new ArrayList<String>();
-		repositories.add("https://github.com/KDE/websites-docs-krita-org");
-		repositories.add("https://github.com/KDE/incidenceeditor");
-		repositories.add("https://github.com/KDE/grantleetheme");
-		repositories.add("https://github.com/KDE/calendarsupport");
-		repositories.add("https://github.com/KDE/kdepim-addons");
-		repositories.add("https://github.com/KDE/krita");
-		repositories.add("https://github.com/KDE/snapcraft-kde-applications");
+//		ArrayList<String> repositories = new ArrayList<String>();
+//		repositories.add("https://github.com/KDE/websites-docs-krita-org");
+//		repositories.add("https://github.com/KDE/incidenceeditor");
+//		repositories.add("https://github.com/KDE/grantleetheme");
+//		repositories.add("https://github.com/KDE/calendarsupport");
+//		repositories.add("https://github.com/KDE/kdepim-addons");
+//		repositories.add("https://github.com/KDE/krita");
+//		repositories.add("https://github.com/KDE/snapcraft-kde-applications");
 
 		ArrayList<Project> projects = ProjectScrapper.scrapper(repositories, "KDE");
 
